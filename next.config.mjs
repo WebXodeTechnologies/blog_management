0ee45cae5c12092ace5967d4b2257d1f916d5c5c
ak220193 <mail2meak22frcrio@gmail.com>
2026-09-06 09:48:11 +0530

    Fix(config): next js config file update

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@ import { withSentryConfig } from "@sentry/nextjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  // Dynamically set output: standalone for Docker, undefined for Vercel deployment
+  output: process.env.VERCEL ? undefined : "standalone",
   reactStrictMode: true,
 
   images: {
